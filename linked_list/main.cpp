7a797e94b59d413cdffddadb9e5cb64cbf3d0ad9
cskilbeck <github@charlie.skilbeck.com>
2014-03-12 23:38:19 +0000

    dfgfsdfg

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stddef.h>
 
 template <typename T> struct list_node_base
 {
@@ -13,37 +12,41 @@ struct linked_list_node
 	list_node_base<T> list_node;
 };
 
+template <typename T, typename R, R T::*M>
+size_t offset_of()
+{
+    return reinterpret_cast<size_t>(&(((T*)0)->*M));
+}
+
 template <typename T, linked_list_node<T> T::*NODE, bool is_member>
 struct list_base
 {
-	template <typename A, typename B>
-	size_t offset_of(B A::*M)
-	{
-	    return reinterpret_cast<size_t>(&(((T*)0)->*M));
-	};
 };
 
 template <typename T, linked_list_node<T> T::*NODE>
 struct list_base<T, NODE, true> : linked_list_node<T>
 {
-	size_t offset()
+	static size_t offset()
 	{
-		return offset_of(&T::*NODE);
+		return offset_of<T, linked_list_node<T>, NODE>();
 	}
 };
 
 template <typename T, linked_list_node<T> T::*NODE>
 struct list_base<T, NODE, false> : linked_list_node<T>
 {
-	size_t offset()
+	static size_t offset()
 	{
-		return offset_of(&T::list_node);
+		return offset_of<T, list_node_base<T>, &T::list_node>();
 	}
 };
 
 template <typename T, linked_list_node<T> T::*NODE = nullptr>
-//struct linked_list : list_base<T, NODE, (linked_list_node<T> T::*)nullptr != NODE>
+#if defined(_MSC_VER)
 struct linked_list : list_base<T, NODE, NODE>
+#elif defined(__GNUC__)
+struct linked_list : list_base<T, NODE, (linked_list_node<T> T::*)nullptr != NODE>
+#endif
 {
 };
 
@@ -61,5 +64,8 @@ linked_list<bar, &bar::node> bar_list;
 
 int main(int, char **)
 {
+	printf("%d\n", bar_list.offset());
+	printf("%d\n", foo_list.offset());
+	getchar();
 	return 0;
 }
