b2872ce831fa4c54323bbd51542da58715eb2bc8
cskilbeck <github@charlie.skilbeck.com>
2014-03-16 07:46:06 +0000

    sort v0.1

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iterator>
 #include <string>
+#include <functional>
 #include <algorithm>
 #include "linked_list.h"
 
@@ -13,16 +14,21 @@ using chs::list_node;
 
 struct foo : list_node<foo>
 {
-    foo(char n) : p(n)
+    foo(char const *n) : p(n)
     {
     }
     ~foo()
     {
     }
-    void knob()
+	std::string to_string() const
+	{
+		return std::string(p);
+	}
+    bool operator < (foo const &a)
     {
+		return strcmp(p, a.p) < 0;
     }
-    char p;
+    char const *p;
 
     list_node<foo> node1;
     list_node<foo> node2;
@@ -33,25 +39,29 @@ struct foo : list_node<foo>
 linked_list<foo> list0;
 
 linked_list<foo, &foo::node1> list1;
-linked_list<foo, &foo::node2> list2;
+linked_list<foo, &foo::node1> list2;
 
 using std::string;
 
 template <typename T> string get(T const &list)
 {
-    string result;
-    for(auto const &i: list)
-    {
-        result += i.p;
-    }
-    return result;
+	string empty;
+	string comma(",");
+	string *sep = &empty;
+	string result("[");
+	for(auto const &i: list)
+	{
+		result += *sep + i.to_string();
+		sep = &comma;
+	}
+	return result + "]";
 }
 
 //////////////////////////////////////////////////////////////////////
 
 template <typename T> void print_list(char const *h, T const &list)
 {
-    printf("%s [%s]\n", h, get(list).c_str());
+    printf("%s %s\n", h, get(list).c_str());
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -69,21 +79,48 @@ template <typename T> void print_list(char const *h, T const &list)
 
 int main(int, char **)
 {
-    foo a('a');
-    foo b('b');
-    foo c('c');
+    foo a("0");
+    foo b("1");
+    foo c("2");
+    foo d("3");
+    foo e("4");
+    foo f("5");
+    foo g("6");
+    foo h("7");
+    foo i("8");
 
-    list1.push_back(a);
-    list1.push_back(b);
+    list1.push_back(d);
     list1.push_back(c);
+    list1.push_back(b);
+    list1.push_back(a);
+    list1.push_back(i);
+    list1.push_back(h);
+    list1.push_back(g);
+    list1.push_back(f);
+    list1.push_back(e);
+
+	list2 = list1.split(i);
 
     print_list("1", list1);
+    print_list("2", list2);
 
-    list1.remove(&a);
-    list1.remove(&b);
-    list1.remove(&c);
+	list1.clear();
+	list2.clear();
 
-    print_list("~1", list1);
+    list1.push_back(d);
+    list1.push_back(c);
+    list1.push_back(b);
+    list1.push_back(a);
+    list1.push_back(i);
+    list1.push_back(h);
+    list1.push_back(g);
+    list1.push_back(f);
+    list1.push_back(e);
+
+	list1.sort();
+
+    print_list("1", list1);
+    print_list("2", list2);
 
     getchar();
     return 0;
