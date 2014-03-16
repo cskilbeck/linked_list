300f0095cddf89a3b770ac4b0517de81c5a4e8b9
cskilbeck <github@charlie.skilbeck.com>
2014-03-16 15:59:21 +0000

    finishing up

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -28,6 +28,10 @@ struct foo : list_node<foo>
     {
 		return strcmp(p, a.p) < 0;
     }
+	bool operator == (char const *c)
+	{
+		return _stricmp(p, c) == 0;
+	}
     char const *p;
 
     list_node<foo> node1;
@@ -39,7 +43,8 @@ struct foo : list_node<foo>
 linked_list<foo> list0;
 
 linked_list<foo, &foo::node1> list1;
-linked_list<foo, &foo::node1> list2;
+linked_list<foo, &foo::node2> list2;
+linked_list<foo, &foo::node1> list3;
 
 //////////////////////////////////////////////////////////////////////
 
@@ -62,23 +67,45 @@ int main(int, char **)
     foo h("7");
     foo i("8");
 
-    //list1.push_back(d);
-    //list1.push_back(c);
-    //list1.push_back(b);
-    //list1.push_back(a);
-    //list1.push_back(i);
-    //list1.push_back(h);
-    //list1.push_back(g);
-    //list1.push_back(f);
-    //list1.push_back(e);
+    list1.push_back(d);
+    list1.push_back(c);
+    list1.push_back(b);
+    list1.push_back(a);
+    list1.push_back(i);
+    list1.push_back(h);
+    list1.push_back(g);
+    list1.push_back(f);
+    list1.push_back(e);
 
     print_list("1", list1);
     print_list("2", list2);
 
+	list1.copy_into(list2);
 	list1.sort();
 
+	list3 = list1.select([] (foo &f) -> bool {
+		return _stricmp(f.p, "4") < 0;
+	});
+
+	foo *p = chs::find_last_of(list3, "2");
+	if(p != nullptr)
+	{
+		printf("Found: %s\n", p->p);
+	}
+
+	list3.for_each([] (foo &f) {
+		if(f.p[0] == '2')
+		{
+			list3.remove(f);
+		}
+		printf("%s\n", f.p);
+	});
+
+	list3.delete_if()
+
     print_list("1", list1);
     print_list("2", list2);
+    print_list("3", list3);
 
     getchar();
     return 0;
